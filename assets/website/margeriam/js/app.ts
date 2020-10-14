e4f566fe8dd704b6e29f60c8e0543e138e09ad32
Anthony Margerand <anthony@lbcd.fr>
2020-10-14 14:10:56 +0200

    Add sentyr

--- assets/website/margeriam/js/app.ts
+++ assets/website/margeriam/js/app.ts
@@ -1,5 +1,16 @@
 import '../css/app.scss';
 import lozad from 'lozad';
+import { init } from '@sentry/browser';
+
+document.addEventListener("DOMContentLoaded", function() {
+  const body = document.querySelector('body') as HTMLBodyElement;
+  const dsn = body.getAttribute('data-sentry');
+  if (dsn) {
+    init({
+      dsn
+    });
+  }
+});
 
 const el = document.querySelectorAll('img');
 const observer = lozad(el);
