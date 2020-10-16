8b6a4647dfdc7258bc624bb2dd239f2303a352c0
Anthony Margerand <anthony@lbcd.fr>
2020-10-16 15:59:17 +0200

    Sentry js

--- assets/website/margeriam/js/app.ts
+++ assets/website/margeriam/js/app.ts
@@ -1,13 +1,18 @@
 import '../css/app.scss';
 import lozad from 'lozad';
 import { init } from '@sentry/browser';
+import { Integrations } from "@sentry/tracing";
 
 document.addEventListener("DOMContentLoaded", function() {
   const body = document.querySelector('body') as HTMLBodyElement;
   const dsn = body.getAttribute('data-sentry');
   if (dsn) {
     init({
-      dsn
+      dsn,
+      integrations: [
+        new Integrations.BrowserTracing(),
+      ],
+      tracesSampleRate: 1.0,
     });
   }
 });
