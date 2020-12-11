148be19c4dd8d820c97fa291d9acc2c4fd138d99
Anthony Margerand <anthony@lbcd.fr>
2020-12-11 22:19:44 +0100

    Lazyload

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,15 @@ Encore
 		// if versioning is enabled, add the file hash too
 		//to: 'images/[path][name].[hash:8].[ext]',
 	})
+	.copyFiles({
+		from: './node_modules/lazysizes',
+
+		// optional target path, relative to the output dir
+		to: 'margeriam/lazysizes/[path][name].[ext]',
+
+		// if versioning is enabled, add the file hash too
+		//to: 'images/[path][name].[hash:8].[ext]',
+	})
 	// only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
