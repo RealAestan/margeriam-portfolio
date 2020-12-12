89c008aa4c05bdba3cb3af8408f22ed58d5446bf
Anthony Margerand <anthony@lbcd.fr>
2020-12-12 19:52:23 +0100

    MediaManager Override

--- src/Controller/Website/CustomController.php
+++ src/Controller/Website/CustomController.php
@@ -6,21 +6,11 @@ namespace App\Controller\Website;
 
 use App\Entity\Contact;
 use App\Form\Type\ContactType;
-use Sulu\Bundle\MediaBundle\Api\Media;
-use Sulu\Bundle\MediaBundle\Media\Exception\FileVersionNotFoundException;
 use Sulu\Bundle\WebsiteBundle\Controller\DefaultController;
 use Sulu\Component\Content\Compat\StructureInterface;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class CustomController extends DefaultController
 {
-    private $cache;
-
-    public function __construct(AdapterInterface $cache)
-    {
-        $this->cache = $cache;
-    }
-
     protected function getAttributes($attributes, StructureInterface $structure = null, $preview = false)
     {
         $contact = new Contact();
@@ -30,35 +20,6 @@ class CustomController extends DefaultController
         ]);
         $attributes = parent::getAttributes($attributes, $structure, $preview);
         $attributes['contactForm'] = $form->createView();
-
-        $localPath = $this->getParameter('sulu_media.media.storage.local.path');
-        if (key_exists('content', $attributes) && key_exists('content', $attributes['content'])) {
-            foreach ($attributes['content']['content'] as $key => $block) {
-                if ($block['type'] === 'imageGallery') {
-                    $itemGallery = $this->cache->getItem($attributes['id'].'_'.$key.'_gallery');
-                    if (!$itemGallery->isHit()) {
-                        die('cc');
-                        $images = [];
-                        /** @var Media $image */
-                        foreach ($block['images'] as $image) {
-                            try {
-                                $fileVersion = $image->getFileVersion();
-                            } catch (FileVersionNotFoundException $exception) {
-                                continue;
-                            }
-                            $storageOptions = $fileVersion->getStorageOptions();
-                            $path = \implode('/', \array_filter([$localPath, $storageOptions['segment'], $storageOptions['fileName']]));
-                            $dimensions = getimagesize($path);
-                            $images[] = new \App\Entity\Media($image, $dimensions[0], $dimensions[1]);
-                        }
-                        $itemGallery->set($images);
-                        $this->cache->save($itemGallery);
-                    }
-                    $images = $itemGallery->get();
-                    $attributes['content']['content'][$key]['images'] = $images;
-                }
-            }
-        }
 
         return $attributes;
     }
